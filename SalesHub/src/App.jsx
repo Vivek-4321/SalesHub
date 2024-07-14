08a6d21035e4344cbf5de530093d55372a111e35
Vivek-4321 <vivekvenugopal514@gmail.com>
2024-07-14 11:37:29 +0530

    Second Commit

--- SalesHub/src/App.jsx
+++ SalesHub/src/App.jsx
@@ -2,6 +2,8 @@ import React from "react"
 import FileUpload from "./FileUpload"
 import Home from "./Home";
 import Login from "./Login";
+import Excel from "./Excel";
+import Calendar from "./Calendar";
 import { Route, Routes } from "react-router-dom";
 
 function App() {
@@ -12,6 +14,8 @@ function App() {
         <Route path='/' element={<Home/>}></Route>
         <Route path='/login' element={<Login/>}></Route>
         <Route path='/dataEntry' element={<FileUpload/>}></Route>
+        <Route path="/excel" element={<Excel/>}></Route>
+        <Route path="/calendar" element={<Calendar/>}></Route>
       </Routes>
     </>
   )
